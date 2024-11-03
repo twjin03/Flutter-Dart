dcd58520471c01924869e10b4be22fea9ed247be
twjin03 <twjin03@g.skku.edu>
2024-11-03 21:17:38 +0900

    104. Creating a Custom List Item with the Card & Spacer Widgets

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,5 +1,5 @@
 
-import 'package:expense_tracker/models/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 

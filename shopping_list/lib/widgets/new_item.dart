3fdd3566bc2458040298def173e760fb9ebfc2f0
twjin03 <twjin03@g.skku.edu>
2025-02-19 21:57:22 +0900

    06 Buttons

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -44,8 +44,8 @@ class _NewItemState extends State<NewItem> {
                     ),
                     const SizedBox(width: 8),
                     Expanded(
-                      child: DropdownButtonFormField(
-                        items: [
+                        child: DropdownButtonFormField(
+                      items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
                               value: category.value,
@@ -55,22 +55,39 @@ class _NewItemState extends State<NewItem> {
                                     width: 16,
                                     height: 16,
                                     color: category.value.color,
-                                  ) ,
-                                  const SizedBox(width: 6,), 
+                                  ),
+                                  const SizedBox(
+                                    width: 6,
+                                  ),
                                   Text(category.value.title),
                                 ],
-                              )
-                            )
-                          ], 
+                              ))
+                      ],
                       onChanged: (value) {},
-                      )
+                    )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Add Item'),
                     ),
                   ],
+                ),
+    
+                  ],
                 )
-              ],
+            ),
           ),
-        )
-      ),
     );
   }
 }
